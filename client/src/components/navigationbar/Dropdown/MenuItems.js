b6c33ada69b7ae160cf174aad53fd78522cf5694
Sophia Sharifi <sophiasharifi@Sophias-MacBook-Pro.local>
2021-01-07 17:05:08 -0800

    removed Icon?

--- client/src/components/navigationbar/Dropdown/MenuItems.js
+++ client/src/components/navigationbar/Dropdown/MenuItems.js
@@ -19,4 +19,22 @@ export const MenuItems = [
         path: '#',
         cName: 'nav-links'
     },
+    {
+        title: 'Brand x',
+        path: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Brand x',
+        path: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Brand x',
+        path: '#',
+        cName: 'nav-links'
+    },
 ]
+
+/* ALL THE DROPDOWN COMPONENTS USE THE SAME MENUITEM.JS FILE CURRENTLY, SO CSS CHANGES MADE TO EACH LINK IN INDIVIDUAL
+ DROPDOWN MENU WILL APPLY TO ALL ... THIS IS A TEMPORARY BUG */
