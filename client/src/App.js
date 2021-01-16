bfb832190548834cc128bc2db4f9ccde2941f9ae
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2021-01-16 10:36:21 -0800

    changing capitilization to match with files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import logo from './DP.png';
-import NavigationBar from './Components/NavigationBar/NavigationBar';
+import NavigationBar from './components/navigationbar/navigationbar';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
