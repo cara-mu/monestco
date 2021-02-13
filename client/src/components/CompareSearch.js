db67792965ce7e47f0b96623401124290b697824
IrenaGao <ijg24@cornell.edu>
2021-02-13 12:25:55 -0500

    more comparison

--- client/src/components/CompareSearch.js
+++ client/src/components/CompareSearch.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../styles/Comparison.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import companies from '../data/companies.json';
+import { CSSTransition } from 'react-transition-group';
 
 var filterBy = [
     'Clothing', 'Sportswear', 'Shoes', 'All'
@@ -14,12 +16,15 @@ class CompareSearch extends React.Component {
             clothing: false,
             sportswear: false,
             shoes: false,
-            all: false
+            all: false,
+            foundCompanies: [],
+            changehandled: false
         };  
         this.toggleClassClothing = this.toggleClassClothing.bind(this);
         this.toggleClassSportswear = this.toggleClassSportswear.bind(this);
         this.toggleClassShoes = this.toggleClassShoes.bind(this);
         this.toggleClassAll = this.toggleClassAll.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     };
 
     toggleClassClothing() {
@@ -39,33 +44,68 @@ class CompareSearch extends React.Component {
         this.setState({all: !currentState});
     }
 
+    handleChange() {
+        console.log("handlechange");
+        var search = document.getElementById("categorysearch").value.toLowerCase();
+        const comps = this.mapCompanies(companies);
+        const foundCompanies = comps.filter(comp => {
+            return comp.toLowerCase().match(search);
+        })
+        this.setState({ 
+            foundCompanies: foundCompanies,
+            changehandled: true
+        });      
+    }
+
+    mapCompanies = (companies) => {
+        console.log("mapcompanies");
+        return companies.companies.map(company => {
+            return company.name;
+        })
+    }
+
+    renderCompanies = (companies) => {
+        console.log("rendercompanies");
+        return companies.map((company, i) => {
+            return <CSSTransition timeout={300}>
+                        <div key={i} className={this.state.changehandled ? 'rendercompanies' : null}>
+                            {company}
+                        </div>  
+                    </CSSTransition>              
+        })
+    }
+
     render() {   
         return (
             <div>
-                <p style={{marginBottom: '9%', fontSize: '18px', color: '#3D3E3F'}}>Filter by:</p>
-                <span 
+                <p style={{marginBottom: '14%', fontSize: '18px', color: '#3D3E3F'}}>Filter by:</p>
+                <span id="filterbutton"
                     className={this.state.clothing ? 'categoryfocus' : null}
                     onClick = {this.toggleClassClothing}
                 >Clothing</span>
-                <span 
+                <span id="filterbutton1"
                     className={this.state.sportswear ? 'categoryfocus' : null}
                     onClick = {this.toggleClassSportswear}
                 >Sportswear</span>
-                <span 
+                <div style={{marginTop: '10%'}}></div>
+                <span id="filterbutton2"
                     className={this.state.shoes ? 'categoryfocus' : null}
                     onClick = {this.toggleClassShoes}
                 >Shoes</span>
-                <span 
+                <span id="filterbutton3"
                     className={this.state.all ? 'categoryfocus' : null}
                     onClick = {this.toggleClassAll}
                 >All</span>
 
-                <hr />
-                <FontAwesomeIcon style={{position: "absolute", marginTop: '22px', marginLeft: '8px', color: 'rgba(50, 50, 50, 0.5'}} icon={faSearch} />
-                <input type="text" onChange={this.handleChange} placeholder="Search the brand" />
+                <hr style={{width: '90%'}}/>
+                <FontAwesomeIcon style={{position: "absolute", marginTop: '21px', marginLeft: '8px', color: 'rgba(50, 50, 50, 0.5'}} icon={faSearch} />
+                <input type="text" id="categorysearch" onChange={this.handleChange} placeholder="Search the brand" style={{width: '80%', height: '23px'}} />
+                <div>
+                    {this.renderCompanies(this.state.foundCompanies)}
+                </div>
             </div>
-        )
-    }
+            )
+        }
 }
 
 export default CompareSearch;
--- client/src/pages/Comparison.js
+++ client/src/pages/Comparison.js
@@ -5,25 +5,42 @@ import Container from 'react-bootstrap/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import '../styles/Comparison.css';
+import CompareTool from '../components/CompareTool';
 
 import CompareSearch from '../components/CompareSearch';
 
 function Comparison(props) {
     return (
         <div>
-            <Grid container spacing={3}>
-                <Grid item xs={4}>
-                    <div style={{marginTop: '15%', marginLeft: '30%'}}>
-                        <p id="brands">Brands</p>
-                        <p style={{marginBottom: '8%', color: '#4F4F4F'}}>Type or click the brands that you want to learn more</p>
+            <Grid container>
+                {/* <Grid item xs={4} style={{border: 'solid'}}> */}
+                <Row style={{marginLeft: '6%', width: '17%', paddingTop: '3%'}}>
+                    {/* <Col style={{marginTop: '25%', marginLeft: '50%', border: 'solid', paddingRight: '20%'}}> */}
+                    <span style={{marginTop: ''}}>
+                        <p id="brands" style={{marginTop: '43%'}}>Brands</p>
+                        <p style={{marginBottom: '8%', color: '#4F4F4F', lineHeight: 1.7, fontSize: '14px'}}>Type or click the brands that you want to learn more about</p>
                         <CompareSearch />
+                    </span>
+                    {/* </Col> */}
+                </Row>
+                {/* </Grid> */}
+                {/* <Grid item xs={8} style={{border: 'solid'}}> */}
+                <Row style={{marginLeft: '5%', width: '70%', border: ''}}>
+                    <div style={{textAlign: 'center', marginTop: '14.5%', marginBottom: '2%', paddingLeft: '6%'}}>
+                        <p id ="apparel">Apparel Comparison Chart</p>
+                        <p style={{lineHeight: '10px'}}>The percentages represent the score brands received based on our methodology.</p>
+                        <p>The higher the percentage, the better the brand is at addressing the issue.</p>
+                        <p id="colors">The colors represent the brand's performance benchmarked to industry standards.</p>
                     </div>
-                </Grid>
-                <Grid item xs={8}>
-                    <div style={{marginTop: '7%', marginLeft: '23%'}}>
-                        Apparel Comparison Chart
-                    </div>
-                </Grid>
+                    <span id="abvavg"></span>
+                    <span className="avgtext"><span style={{fontWeight: 'bold'}}>Above</span> Average</span> 
+                    <span id="avg"></span>
+                    <span className="avgtext">Average</span>
+                    <span id="blwavg"></span>
+                    <span className="avgtext"><span style={{fontWeight: 'bold'}}>Below</span> Average</span>
+                {/* </Grid> */}
+                    <CompareTool />
+                </Row>
             </Grid>
         </div>
     )
