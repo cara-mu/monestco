62dfbaeb60b7c8cc883e74ddd80df18e1cc13722
m-hammad-ali <mohammadhammad0358@gmail.com>
2021-03-11 22:28:16 +0500

    two new pages added

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import Comparison from './pages/Comparison';
 
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Methodology from './pages/Methodology';
+import BrandBreakdown from './pages/BrandBreakdown';
 
 function App() {
   return (
     <Router>
       <NavigationBar />
       <Route path="/comparison" exact render={() => <Comparison />} />
+      <Route path="/methodology" exact render ={()=> <Methodology/>}/>
+      <Route path="/brand-breakdown" exact render ={()=> <BrandBreakdown/>}/>
     </Router>
   );
 }
