69b20de7d8038210c2946dc98a09058485fcc7a6
Sophia Sharifi <sophiasharifi@Sophias-MacBook-Pro.local>
2021-03-13 22:32:32 -0800

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -4,35 +4,27 @@ import NavigationBar from './components/NavigationBar';
 import Comparison from './pages/Comparison';
 
 import './App.css';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Company from './components/Companies/Company';
-import HomePage from './components/Home/HomePage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Methodology from './pages/Methodology';
 import BrandBreakdown from './pages/BrandBreakdown';
 import BrandDirectory from './pages/BrandDirectory';
+import Company from './components/Companies/Company';
+import HomePage from './components/Home/HomePage';
 
->>>>>>> d053cabcc7eacb404c6d2c88a7e8f752977049b1
 function App() {
   return (
     <Router>
       <NavigationBar />
-<<<<<<< HEAD
       <Switch>
         <Route path = '/' exact component = {HomePage}/>
       </Switch>
       <Switch>
         <Route path = '/Companies/:companyName' exact component = {Company}/>
       </Switch>
-=======
-      <Route path="/comparison" exact render={() => <Comparison />} />
-      <Route path="/methodology" exact render ={()=> <Methodology/>}/>
+      <Route path="/comparison" exact component = {Comparison} />
+      <Route path="/methodology" exact component ={Methodology}/>
       <Route path="/brand-breakdown" exact render ={()=> <BrandBreakdown/>}/>
       <Route path="/brand-directory" exact render={()=><BrandDirectory/>}/>
->>>>>>> d053cabcc7eacb404c6d2c88a7e8f752977049b1
     </Router>
   );
 }
