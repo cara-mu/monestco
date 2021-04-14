7e3ab0acb124c1f09218c5fe5d79c24961984743
sophia sharifi <sophiasharifi@sophias-MacBook-Pro.local>
2021-04-14 11:13:20 -0700

    company route

--- server/server.js
+++ server/server.js
@@ -27,7 +27,26 @@ app.get("/companies", (req, res, next) => {
       }
   });
 });
+app.get('/', (req, res) => {
+  res.status(200).send('Monest Home Page!')
+})
 
+// This would just be the page for singular company
+app.get('/companies/:companyName', function(req,res,next){
+  var companyName = req.params.companyName;
+
+  db.get("SELECT * FROM companies WHERE name = ?", [companyName], (err, row) => {
+    if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+    }
+    if(row){
+      res.status(200).json(row);
+    }
+
+    next();
+});
+})
 // Error page
 app.get('*', function(req, res) {
   res.status(404).send("Error Page")
