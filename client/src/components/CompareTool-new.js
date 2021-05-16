d51faaf591e4c41d904c8316362c497f88bccf20
neel <neelgaywala72@gmail.com>
2021-05-16 22:16:37 +0530

    fix

--- client/src/components/CompareTool-new.js
+++ client/src/components/CompareTool-new.js
@@ -939,7 +939,7 @@ const ScoreContainer = ({
     // console.log(score);]
     if (score > industrialStandard[1]) {
       return (
-        <div className="compare-card-container">
+        <div className={`compare-card-container ${!firstLayer && !secondLayer && "greensquare2"}`}>
           {!showLearnMoreText && (
             <div
               className={
@@ -974,7 +974,7 @@ const ScoreContainer = ({
       // g
     } else if (score < industrialStandard[1] && score > industrialStandard[0]) {
       return (
-        <div className="compare-card-container">
+        <div className={`compare-card-container ${!firstLayer && !secondLayer && "yellowsquare2"}`}>
           {!showLearnMoreText && (
             <div
               className={
@@ -1008,7 +1008,7 @@ const ScoreContainer = ({
       );
     } else {
       return (
-        <div className="compare-card-container">
+        <div className={`compare-card-container ${!firstLayer && !secondLayer && "orangesquare2"}`}>
           {!showLearnMoreText && (
             <div
               className={
