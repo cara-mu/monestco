77fd6687c1bb7e13e74bcbe15a87a10560fa33bf
IrenaGao <ijg24@cornell.edu>
2021-05-21 00:09:24 -0400

    b

--- server/server.js
+++ server/server.js
@@ -779,6 +779,16 @@ app.get('/comparison/:companyID1/:companyID2/:companyID3', function(req,res,next
 // });
 
 // Error page
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
 app.get('*', function(req, res) {
   res.status(404).send("Error Page")
 })
