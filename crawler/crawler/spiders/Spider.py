5a896c699bf26fb1f03f5a9b6097746143158b84
Justin Hu <geek.huqijun@gmail.com>
2022-01-07 17:44:40 -0500

    Crawler 1. Automatic Link Extraction and Follow, specified by Rules 2. Use of Playwright configurable, speciafied by configuration

--- crawler/crawler/spiders/Spider.py
+++ crawler/crawler/spiders/Spider.py
@@ -1,41 +1,31 @@
 from scrapy import Spider, Request
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.crawler import CrawlerProcess
-from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.linkextractors import LinkExtractor
 from scrapy_playwright.page import PageCoroutine
 from scrapy.utils.response import open_in_browser
 
+Detail_Page_Rendering_Required = False
 
-class MonestSpider(Spider):
+class MonestSpider(CrawlSpider):
     name = 'MonestSpider'
+    start_urls = ['https://www.adidas.com/us/clothing?grid=true',]
 
-    def start_requests(self):
-        urls = ['https://www.adidas.ca/en/clothing',]
+    detail_callback = 'sync_parse_detail_page'
+    if Detail_Page_Rendering_Required:
+        detail_callback = 'async_parse_detail_page'
 
-        for url in urls:
-            yield Request(url, callback=self.parse_list_page)
 
-    def parse_list_page(self, response, **kwargs):
-        #open_in_browser(response)
-        link_extractor = LxmlLinkExtractor(restrict_css='a[data-auto-id=glass-hockeycard-link]')
-        for link in link_extractor.extract_links(response):
-            yield Request(link.url,
-                          callback=self.parse_detail_page,
-                          meta=dict(
-                              playwright=True,
-                              plawright_include_page=True,
-                              playwright_page_coroutines=[
-                                  PageCoroutine("wait_for_selector",
-                                                "div[class*=sidebar-wrapper] h1[data-auto-id=product-title] span"),
-                                  PageCoroutine("wait_for_selector",
-                                                "div[class*=sidebar-wrapper] div[class*=product-price] div["
-                                                "class*=gl-price-item]"),
-                                  PageCoroutine("wait_for_selector", "div[data-auto-id=product-category] span"),
-                                  PageCoroutine("wait_for_selector", "div#navigation-target-description p"),
-                              ],
-                          ))
+    rules = (
+        # following list pages of clothings
+        Rule(LinkExtractor(allow=('clothing\?grid=true&start'))),
+        # Extract Item detail pages
+        Rule(LinkExtractor(restrict_css='a[data-auto-id=glass-hockeycard-link]', ),
+             callback=detail_callback,
+             process_request='process_detail_request')
+    )
 
-    async def parse_detail_page(self, response):
-        #open_in_browser(response)
+    def sync_parse_detail_page(self, response):
         yield {
             'name': response.css('div[class*=sidebar-wrapper] h1[data-auto-id=product-title] span::text').get(),
             'price': response.css('div[class*=sidebar-wrapper] div[class*=product-price] div['
@@ -43,6 +33,31 @@ class MonestSpider(Spider):
             'category': response.css('div[data-auto-id=product-category] span::text').get(),
             'description': response.css('div#navigation-target-description p::text').get(),
         }
+
+    async def async_parse_detail_page(self, response):
+        # open_in_browser(response)
+        yield {
+            'name': response.css('div[class*=sidebar-wrapper] h1[data-auto-id=product-title] span::text').get(),
+            'price': response.css('div[class*=sidebar-wrapper] div[class*=product-price] div['
+                                  'class*=gl-price-item]::text').get(),
+            'category': response.css('div[data-auto-id=product-category] span::text').get(),
+            'description': response.css('div#navigation-target-description p::text').get(),
+        }
+
+    def process_detail_request(self, request, response):
+        #enble playwright rendering
+        if Detail_Page_Rendering_Required:
+            current_meta = request.meta
+            current_meta |= dict(
+                playwright=True,
+                playwright_page_coroutines=[
+                    PageCoroutine('wait_for_selector', 'div#navigation-target-description p'),
+                ]
+            )
+
+        return request
+
+
 
 if __name__ == "__main__":
     process = CrawlerProcess(
@@ -52,6 +67,7 @@ if __name__ == "__main__":
                 "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
                 # "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
             },
+            'PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT': 0,
         }
     )
     process.crawl(MonestSpider)
