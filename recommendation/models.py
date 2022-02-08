11090ae2c5cf0ac781d207bfa5b91ba6b7da5c3e
Justin Hu <geek.huqijun@gmail.com>
2022-02-07 19:47:46 -0500

    1. H&M downloader/crawler utilizing API. 2. Downloader write data to database 3. update Product model for recommendation, adding id, created_at, updated_at to facilitate data update in the future

--- recommendation/models.py
+++ recommendation/models.py
@@ -4,10 +4,13 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, blank=False)
+    id = models.CharField(max_length=50, primary_key=True)
     title = models.CharField(max_length=100, blank=True)
-    sub_title = models.CharField(max_length=100, blank=True)
+    category = models.CharField(max_length=100, blank=True)
     price = models.FloatField(blank=True)
-    color = models. CharField(max_length=50, blank=True)
-    url = models.URLField(max_length=300, unique=True)
-    photo = models.URLField(max_length=300, blank=True)
+    color = models.CharField(max_length=200, blank=True)
+    descr = models.CharField(max_length=1000, blank=True)
+    url = models.URLField(max_length=300)
+    image = models.URLField(max_length=300, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
