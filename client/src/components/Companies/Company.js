c0d1a609acd4a12d0643670a8ae57cfa81873f0d
tracywanggg <tracywangk@gmail.com>
2022-03-04 19:44:37 -0500

    Component for Similar Brands

--- client/src/components/Companies/Company.js
+++ client/src/components/Companies/Company.js
@@ -24,6 +24,7 @@ import PoliticalContribution from "./PoliticalContribution";
 import Facts from "./Facts";
 import Scores from "./Scores/Scores";
 import BrandProfile from "./BrandProfile";
+import SimilarBrand from "./SimilarBrand";
 
 const companyinfo = [
     {
@@ -666,100 +667,7 @@ function Company({ match, location }) {
 
                         {/* Similar Brands */}
 
-
-                        <div className="similar_brands">
-                            <div className='Brand-Section-title'>Similar Brands</div>
-                            <div className='Decorative-Line'></div>
-                            <div className="similar_brands-container">
-                                <div>
-                                    <Link className='Similar-Link' to={'/companies/' + companyDetails[0]["SimilarCompany1"]}>
-                                        <p>{companyDetails[0]["SimilarCompany1"]}</p>
-                                        {/* <div>{companyDetails[0]["SimilarCompany1"]}</div> */}
-                                        <div className='brand_box'>
-                                            {/* <div className="d-fdlex justify-content-center">
-                                            <div style={{fontFamily: 'DM Sans', fontWeight: 500, fontSize: '14px', marginTop: '8%', textAlign: 'center'}}>industry average</div>
-                                            <div style={{textAlign: 'center'}}><AiFillCaretDown style={{}}/></div>
-                                            <div class="horizontalline2" style={{width: `${companyDetails[0]["SliderLength"]}px` }}></div>
-                                            <div class="verticalline2"></div> 
-                                            <img src="https://github.com/sophiasharifi/monestco/blob/main/images/slider%20backgroud.png?raw=true" className="brand_logo"/> */}
-                                            {/* <p style={{fontFamily: 'DM Sans', fontSize: '12px', color: '#4F4F4F'}} >underperforming<span style={{marginLeft: '145px'}}>overperforming</span></p> */}
-                                            <div className="d-fledx justify-content-center" style={{ position: "relative" }}>
-                                                <div style={{ fontFamily: 'DM Sans', fontWeight: 500, fontSize: '14px', marginTop: '8%', textAlign: 'center' }}>industry average</div>
-                                                <div style={{ textAlign: 'center' }}><AiFillCaretDown style={{}} /></div>
-                                                <div class="horizontalline2" style={{ width: `${companyDetails[0]["comp1SliderLength"]}px` }}></div>
-                                                <div class="verticalline2"></div>
-                                                <img src="https://github.com/sophiasharifi/monestco/blob/main/images/slider%20backgroud.png?raw=true" className="brand_logo" />
-                                                <div className="performing-Container"><span>underperforming</span><span>overperforming</span></div>
-                                                {/* <p style={{fontFamily: 'DM Sans', fontSize: '12px', color: '#4F4F4F'}} >underperforming<span style={{marginLeft: '145px'}}>overperforming</span></p> */}
-                                            </div>
-                                            <div className="brand_inside_text ml-10perc">
-                                                <span>{Math.round(companyDetails[0]["company1TotalScore"], 2)}</span>
-                                                <span>/100</span>
-                                            </div>
-                                            {/* </div> */}
-                                        </div>
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Link className='Similar-Link' to={'/companies/' + companyDetails[0]["SimilarCompany2"]}><p>{companyDetails[0]["SimilarCompany2"]}</p>
-                                        {/* <div>{companyDetails[0]["SimilarCompany2"]}</div> */}
-                                        <div className='brand_box'>
-                                            <div className="d-fldex justify-content-center" style={{ position: "relative" }}>
-                                                <div style={{ fontFamily: 'DM Sans', fontWeight: 500, fontSize: '14px', marginTop: '8%', textAlign: 'center' }}>industry average</div>
-                                                <div style={{ textAlign: 'center' }}><AiFillCaretDown style={{}} /></div>
-                                                <div class="horizontalline2" style={{ width: `${companyDetails[0]["comp2SliderLength"]}px` }}></div>
-                                                <div class="verticalline2"></div>
-                                                <img src="https://github.com/sophiasharifi/monestco/blob/main/images/slider%20backgroud.png?raw=true" className="brand_logo" />
-                                                <div className="performing-Container"><span>underperforming</span><span>overperforming</span></div>
-                                            </div>
-                                            <div className="brand_inside_text ml-10perc">
-                                                <span>{Math.round(companyDetails[0]["company2TotalScore"], 2)}</span>
-                                                <span>/100</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Link className='Similar-Link' to={'/companies/' + companyDetails[0]["SimilarCompany3"]}><p>{companyDetails[0]["SimilarCompany3"]}</p>
-                                        {/* <div>{companyDetails[0]["SimilarCompany3"]}</div> */}
-                                        <div className='brand_box'>
-                                            <div className="d-fldex justify-content-center" style={{ position: "relative" }}>
-                                                <div style={{ fontFamily: 'DM Sans', fontWeight: 500, fontSize: '14px', marginTop: '8%', textAlign: 'center' }}>industry average</div>
-                                                <div style={{ textAlign: 'center' }}><AiFillCaretDown style={{}} /></div>
-                                                <div class="horizontalline2" style={{ width: `${companyDetails[0]["comp3SliderLength"]}px` }}></div>
-                                                <div class="verticalline2"></div>
-                                                <img src="https://github.com/sophiasharifi/monestco/blob/main/images/slider%20backgroud.png?raw=true" className="brand_logo" />
-                                                <div className="performing-Container"><span>underperforming</span><span>overperforming</span></div>
-                                            </div>
-                                            <div className="brand_inside_text ml-10perc">
-                                                <span>{Math.round(companyDetails[0]["company3TotalScore"], 2)}</span>
-                                                <span>/100</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Link className='Similar-Link' to={'/companies/' + companyDetails[0]["SimilarCompany4"]}><p>{companyDetails[0]["SimilarCompany4"]}</p>
-                                        {/* <div>{companyDetails[0]["SimilarCompany4"]}</div> */}
-                                        <div className='brand_box'>
-                                            <div className="d-fldex justify-content-center" style={{ position: "relative" }}>
-                                                <div style={{ fontFamily: 'DM Sans', fontWeight: 500, fontSize: '14px', marginTop: '8%', textAlign: 'center' }}>industry average</div>
-                                                <div style={{ textAlign: 'center' }}><AiFillCaretDown style={{}} /></div>
-                                                <div class="horizontalline2" style={{ width: `${companyDetails[0]["comp4SliderLength"]}px` }}></div>
-                                                <div class="verticalline2"></div>
-                                                <img src="https://github.com/sophiasharifi/monestco/blob/main/images/slider%20backgroud.png?raw=true" className="brand_logo" />
-                                                <div className="performing-Container"><span>underperforming</span><span>overperforming</span></div>
-                                            </div>
-                                            <div className="brand_inside_text ml-10perc">
-                                                <span>{Math.round(companyDetails[0]["company4TotalScore"], 2)}</span>
-                                                <span>/100</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                                {/* </div> */}
-                            </div>
-                        </div>
+                        <SimilarBrand company = {companyName}/>
                     </div>
                 </Grid>
             </Grid>
