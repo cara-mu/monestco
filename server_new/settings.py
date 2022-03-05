3c7754710b19d03e065d9c7a94d7ee0a2547ec72
Austin6868 <zzha63@uwo.ca>
2022-03-05 13:29:03 -0500

    Credential changes & more

--- server_new/settings.py
+++ server_new/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'monest',
-        'USER': 'monest',
+        'USER': 'austinzhang',
         'PASSWORD': 'monest@123',
         'HOST': 'localhost',
         'PORT': '',
